457c7de86e3ed6cdf28f2e9d4fcdc759409d8818
Rachael Drennan <rdrennan0@gmail.com>
2019-02-02 16:52:08 -0700

    partially fix categories issue

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -23,7 +23,6 @@ export default {
       'American Cities',
       'Food',
       'Cable TV'];
-    // console.log(firstClue);
     
     for (let ind = 1; ind < 5; ind++)  {
       let firstClue = game.rounds[0].roundClues[ind - 1][0].categoryId;
@@ -31,35 +30,35 @@ export default {
     }
   },
   GameBoardListener(event) {
-    console.log('testing2');
     if (event.target.tagName.toLowerCase() === 'h2') {
       event.target = event.target.parentElement
     }
     let classItem = event.target.className;
-    console.log(classItem)
     let currentQuestion; 
     let categoryIndex = event.target.classList[1];
     switch (true) {
     case classItem.includes('100-val'):
       currentQuestion = game.rounds[0].roundClues[categoryIndex][0];
       event.target.classList.add('question-used');
+      console.log(currentQuestion);
       break;
     case classItem.includes('200-val'):
       currentQuestion = game.rounds[0].roundClues[categoryIndex][1];
       event.target.classList.add('question-used');
+      console.log(currentQuestion);
       break;
     case classItem.includes('300-val'):
       currentQuestion = game.rounds[0].roundClues[categoryIndex][2];
-      console.log('YUUUUP');
       event.target.classList.add('question-used');
+      console.log(currentQuestion);
       break;
     case classItem.includes('400-val'):
       currentQuestion = game.rounds[0].roundClues[categoryIndex][3];
       event.target.classList.add('question-used');
+      console.log(currentQuestion);
       break;
     }
-    console.log(game.rounds[0].roundClues[1][1])
-    console.log(currentQuestion)
+
   }
 }
 
